75aa78c605a2d9af291f9ebe10b677986d701c1f
Jose Cabral <jose.cabral@adclick.pt>
2017-10-26 17:31:28 +0100

    Prepare PHP7.1

--- lib/task/sfPropelDiffTask.class.php
+++ lib/task/sfPropelDiffTask.class.php
@@ -8,12 +8,12 @@
  * file that was distributed with this source code.
  */
 
-require_once(dirname(__FILE__).'/sfPropelBaseTask.class.php');
-require_once('generator/lib/model/AppData.php');
-require_once('generator/lib/builder/util/XmlToAppData.php');
-require_once('generator/lib/platform/DefaultPlatform.php');
-require_once('generator/lib/model/diff/PropelDatabaseComparator.php');
-require_once('generator/lib/util/PropelMigrationManager.php');
+require_once (dirname(__FILE__).'/sfPropelBaseTask.class.php');
+require_once (sfConfig::get('sf_propel_path').'/generator/lib/model/AppData.php');
+require_once (sfConfig::get('sf_propel_path').'/generator/lib/builder/util/XmlToAppData.php');
+require_once (sfConfig::get('sf_propel_path').'/generator/lib/platform/DefaultPlatform.php');
+require_once (sfConfig::get('sf_propel_path').'/generator/lib/model/diff/PropelDatabaseComparator.php');
+require_once (sfConfig::get('sf_propel_path').'/generator/lib/util/PropelMigrationManager.php');
 
 /**
  * Create classes for the current model.
